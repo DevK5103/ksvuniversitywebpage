758f53270d9e421d5a7be0125f13f9f5b59f0a72
Aayush Pandya <200375665+pandyaaayush04@users.noreply.github.com>
2026-03-25 01:03:00 +0530

    Fixed navbar overlap, implemented transparency, and updated images with local assets

--- src/components/home/PresidentMessage.jsx
+++ src/components/home/PresidentMessage.jsx
@@ -37,7 +37,7 @@ export default function PresidentMessage() {
               
               <div className="font-body text-ksv-dark/80 text-lg italic leading-relaxed mb-8 space-y-4">
                 <p>
-                  "Education is not merely about acquiring degrees; it is about building character and empowering individuals to contribute meaningfully to society. At K.S.V., we are committed to this holistic development..."
+                  "Sarva Vidyalaya Kelvani Mandal, a Public Charitable Trust has been established in the year 1919 with the Principle of “KAR BHALA HOGA BHALA” as preached by the founder Pujya Chhaganbha. The trust has been in the forefront of providing quality education since inception. With only 6 students, the trust commenced providing education in the Parched North Gujarat Area and had branches across North Gujarat at places like Kadi, Visnagar, Vadnagar, Balol, Gandhinagar etc. With the objective of providing education to one and all the trust has been managing various schools right from Pre Primary Level to Primary to Secondary to Higher Secondary level both in Gujarati Medium and English Medium.  In addition to the schools the trust also manages a University i.e., Kadi Sarva Vishwavidyalaya and also other colleges."
                 </p>
               </div>
               
